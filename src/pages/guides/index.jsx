5bacec5997f08c69848f486222857937c9d6adc0
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-12-23 14:53:46 +0100

    fix guide

--- src/pages/guides/index.jsx
+++ src/pages/guides/index.jsx
@@ -28,6 +28,21 @@ function Guide() {
     fetchPosts();
   }, []);
 
+  function addOrdinalSuffix(number) {
+    const j = number % 10,
+      k = number % 100;
+    if (j === 1 && k !== 11) {
+      return number + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return number + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return number + "rd";
+    }
+    return number + "th";
+  }
+
   return (
     <main className="w-full h-full">
       {loading && <Loader />}
@@ -42,21 +57,40 @@ function Guide() {
           <DownArrow />
         </div>
       </div>
-      <div className="p-20 grid md:grid-cols-2 gap-5">
+      <div
+        className="md:p-20 grid md:grid-cols-2 md:gap-5"
+        data-aos="fade-up"
+        data-aos-duration="950"
+        data-aos-easing="ease-in-out"
+      >
         {posts.map((post) => (
           <Link
             to={`/guides/${post.id}`}
             key={post.id}
-            className="shadow p-5 flex items-center justify-between"
+            className="shadow-lg border bg-white p-5 md:w-[40vw] flex items-center justify-start gap-6 "
           >
             <div>
+              <img
+                src={post.content.rendered.match(/src="([^"]+)"/)[1]}
+                alt="guide image"
+                className="w-50 h-40 object-cover"
+              />
+            </div>
+            <div className="w-[4rem] h-40 md:pt-[2rem] pt-6 px-5">
               <span>
+                <span style={{ color: "blue" }}>
+                  {addOrdinalSuffix(
+                    DateTime.fromISO(post.date, { locale: "en" }).day
+                  )}
+                </span>{" "}
                 {DateTime.fromISO(post.date, { locale: "en" }).toFormat(
-                  "d 'of' LLL yyyy"
+                  "LLL yyyy"
                 )}
               </span>
             </div>
-            <div>{post.title.rendered}</div>
+            <div className="border-l-2 p-5 w-[14rem] h-40 md:p-[2rem]">
+              <p>{post.title.rendered}</p>
+            </div>
           </Link>
         ))}
       </div>
